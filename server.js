36150015baa080401491a0035549dd0371457035
Landon Evans <evans.landon@gmail.com>
2019-09-07 11:44:09 -0500

    finished

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-Dependencies
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 9000;
 
 // For serving of static CSS
 app.use(express.static(__dirname + "/app/css"));
